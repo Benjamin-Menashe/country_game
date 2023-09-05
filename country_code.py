6a0fe94944f8199b4dc5953bbee995a7e8cdaecf
Benjamin Menashe <81152368+Benjamin-Menashe@users.noreply.github.com>
2023-09-05 21:32:02 +0200

    Update country_code.py

--- country_code.py
+++ country_code.py
@@ -14,7 +14,7 @@ def delete_game_copy():
         os.remove('current_game_countries.csv')
 
 # Load the CSV file containing the countries and their first/last letters
-@st.cache_resource
+@st.cache
 def load_country_data():
     return pd.read_csv('current_game_countries.csv') if os.path.exists('current_game_countries.csv') else pd.DataFrame()
 
@@ -41,22 +41,13 @@ def suggest_country(input_country):
     if last_letter in letter_bank:
         available_countries = country_data[country_data['first_letter'] == last_letter]
         if len(available_countries) > 0:
-            # Find the country with the lowest value of "last_letter" in the letter bank
-            min_last_letter_count = float('inf')
-            suggested_country = ""
-            for country in available_countries['country'].tolist():
-                country_last_letter_count = letter_bank[country[-1]]
-                if country_last_letter_count < min_last_letter_count:
-                    min_last_letter_count = country_last_letter_count
-                    suggested_country = country
-
-            if suggested_country:
-                country_data.drop(country_data[country_data['country'] == suggested_country].index, inplace=True)
-                letter_bank[last_letter] -= 1
-                if letter_bank[last_letter] == 0:
-                    del letter_bank[last_letter]
-                save_game_copy()  # Save the updated data to the copy CSV file
-                return suggested_country
+            country = random.choice(available_countries['country'].tolist())
+            country_data.drop(country_data[country_data['country'] == country].index, inplace=True)
+            letter_bank[last_letter] -= 1
+            if letter_bank[last_letter] == 0:
+                del letter_bank[last_letter]
+            save_game_copy()  # Save the updated data to the copy CSV file
+            return country
     return "No country found for that letter."
 
 # Streamlit UI
