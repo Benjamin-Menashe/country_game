c56f1460ce1c1428cdcd992534972eff8f0b2419
Benjamin Menashe <81152368+Benjamin-Menashe@users.noreply.github.com>
2023-09-05 22:38:09 +0200

    Update country_code.py

--- country_code.py
+++ country_code.py
@@ -48,7 +48,11 @@ def suggest_country(input_country):
             min_last_letter_count = float('inf')
             suggested_country = ""
             for country in available_countries['country'].tolist():
-                suggested_country = random.choice(available_countries['country'].tolist())
+                if country not in played_countries:
+                    count = letter_bank.get(country[-1], float('inf'))
+                    if count < min_last_letter_count:
+                        min_last_letter_count = count
+                        suggested_country = country
 
 
             if suggested_country:
