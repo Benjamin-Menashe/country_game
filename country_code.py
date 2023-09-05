fb97e733cad48f8adf40607db2fb280f0cee06de
Benjamin Menashe <81152368+Benjamin-Menashe@users.noreply.github.com>
2023-09-05 21:12:05 +0300

    Add files via upload

---
+++ country_code.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  5 20:05:56 2023
+
+@author: Benjamin
+"""
+
+import streamlit as st
+import pandas as pd
+import random
+import shutil  # For copying the CSV file
+import os     # For file deletion
+
+# Function to create a copy of the CSV file for the current game
+def create_game_copy():
+    shutil.copy('countries.csv', 'current_game_countries.csv')
+
+# Function to delete the copy of the CSV file for the current game
+def delete_game_copy():
+    if os.path.exists('current_game_countries.csv'):
+        os.remove('current_game_countries.csv')
+
+# Load the CSV file containing the countries and their first/last letters
+@st.cache
+def load_country_data():
+    return pd.read_csv('current_game_countries.csv') if os.path.exists('current_game_countries.csv') else pd.DataFrame()
+
+# Function to save the modified data back to the copy CSV file
+def save_game_copy():
+    country_data.to_csv('current_game_countries.csv', index=False)
+
+# Initialize the copy of the CSV file for the current game
+create_game_copy()
+
+# Create a letter bank to count and sort the letters
+letter_bank = {}
+country_data = load_country_data()  # Load the copy for the current game
+
+for _, row in country_data.iterrows():
+    first_letter = row['first_letter']
+    if first_letter not in letter_bank:
+        letter_bank[first_letter] = 0
+    letter_bank[first_letter] += 1
+
+# Function to suggest a country based on the last letter of the input country
+def suggest_country(input_country):
+    last_letter = input_country[-1].lower()
+    if last_letter in letter_bank:
+        available_countries = country_data[country_data['first_letter'] == last_letter]
+        if len(available_countries) > 0:
+            country = random.choice(available_countries['country'].tolist())
+            country_data.drop(country_data[country_data['country'] == country].index, inplace=True)
+            letter_bank[last_letter] -= 1
+            if letter_bank[last_letter] == 0:
+                del letter_bank[last_letter]
+            save_game_copy()  # Save the updated data to the copy CSV file
+            return country
+    return "No country found for that letter."
+
+# Streamlit UI
+st.title("Country Game")
+
+# User input for a country
+input_country = st.text_input("Enter a country:", "").strip()
+
+# Initialize turn counter
+if 'turn' not in st.session_state:
+    st.session_state.turn = 0
+
+if input_country:
+    if input_country in country_data['country'].tolist():
+        country_data.drop(country_data[country_data['country'] == input_country].index, inplace=True)
+        last_letter = input_country[-1].lower()
+        if last_letter in letter_bank:
+            letter_bank[last_letter] -= 1
+            if letter_bank[last_letter] == 0:
+                del letter_bank[last_letter]
+        suggested_country = suggest_country(input_country)
+        st.write(f"Suggested Country: {suggested_country}")
+        
+        # Increment the turn counter
+        st.session_state.turn += 1
+    else:
+        st.write("Wrong input. The country is not in the current country bank.")
+
+# Display the turn count
+st.write(f"Turns taken: {st.session_state.turn}")
+
+# Reset Game button
+if st.button("Reset Game"):
+    delete_game_copy()
+    create_game_copy()
+    st.write("Game has been reset. Start a new game!")
+
+# Add an option to manually delete the copy file
+if st.button("Delete Game Data"):
+    delete_game_copy()
+    st.write("Game data has been deleted.")
