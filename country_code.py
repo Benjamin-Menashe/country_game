6964e5faeb9f6466233eaa52c3170677f594a3eb
Benjamin Menashe <81152368+Benjamin-Menashe@users.noreply.github.com>
2023-09-05 21:09:29 +0200

    Update country_code.py

--- country_code.py
+++ country_code.py
@@ -94,7 +94,7 @@ if input_country:
 # Display the turn count
 st.write(f"Turns taken: {st.session_state.turn}")
 
-st.write(country_data['country'].str.tolist())
+st.write(country_data['country'].str.tolist()[0])
 
 # Reset Game button
 if st.button("Reset Game"):
